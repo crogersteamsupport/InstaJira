2cef51b3e86fecb447b10ea8c01ca8065db7d7f9
Scot Prichard <sprichard@teamsupport.com>
2018-12-18 15:00:16 -0600

    update actionID

--- WebApp/App_Code/TicketPageService.cs
+++ WebApp/App_Code/TicketPageService.cs
@@ -1846,7 +1846,7 @@ namespace TSWebServices
             Model_API.ReadActionAttachmentsForTicket(insertedKBTicketID, ActionAttachmentsByTicketID.KnowledgeBase, out results);
             foreach (AttachmentProxy attachment in results)
             {
-                attachment.AttachmentID = actionID; // clone attachment from KB attachment to this action
+                attachment.AttachmentID = 0; // not used
                 string originalAttachmentRefID = ((ActionAttachmentProxy)attachment).ActionID.ToString();
                 string clonedActionAttachmentPath = attachment.Path.Substring(0, attachment.Path.IndexOf(@"\Actions\") + @"\Actions\".Length)
                                 + actionID.ToString()
@@ -1855,6 +1855,7 @@ namespace TSWebServices
                     Directory.CreateDirectory(Path.GetDirectoryName(clonedActionAttachmentPath));
                 File.Copy(attachment.Path, clonedActionAttachmentPath);
                 attachment.Path = clonedActionAttachmentPath;
+                attachment.RefID = actionID;
 
                 Model_API.Create(attachment);
             }
